cafae091af19dec37ae3612872af16852c388b5a
Luis Munoz <lmumo95@gmail.com>
2021-11-24 17:46:18 -0500

    Creating a fake footer to have a better visibility at the end

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 
 import Billboard from '../../components/billboard/Billboard';
+import Footer from '../../components/footer/Footer';
 import MovieList from '../../components/movie-list/MovieList';
 import Navbar from '../../components/navbar/Navbar';
 import './home.scss';
@@ -13,6 +14,7 @@ const Home = () => {
             {MovieCategories?.map((category, index) => {
                  return (<MovieList key={index} category={category.title} requestURL={category.url} />);
             })}
+            <Footer></Footer>
 
         </div>
     )
